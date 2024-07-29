e11a6b89c97dc9ef61348e046a41c7a5d0f0c0de
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-07-30 00:40:21 +0700

    checking login or not

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,6 +9,10 @@ class AuthController extends Controller
 {
     public function showLoginForm()
     {
+        if (Auth::check()) {
+            return redirect()->route('dashboard'); 
+        }
+
         return view('auth.login');
     }
 
