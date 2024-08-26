dbecd6811ee5be07a2f369c834f415a0876176a1
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-08-26 23:37:48 +0700

    modify bar input search

--- resources/views/dashboard/pages/resources/pages/documents.blade.php
+++ resources/views/dashboard/pages/resources/pages/documents.blade.php
@@ -30,8 +30,7 @@
                 </div>
                 
                 <div class="flex items-end">
-                    <button type="submit"
-                        class="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300">Filter</button>
+                    <button type="submit" class="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300">Filter</button>
                 </div>
             </form>
         </div>
