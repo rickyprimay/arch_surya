711e3e93a45591f8b8555fd262a3fda3e55e1766
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-08-27 20:06:14 +0700

    giving edit information on report

--- resources/views/dashboard/pages/resources/pages/timeline.blade.php
+++ resources/views/dashboard/pages/resources/pages/timeline.blade.php
@@ -76,11 +76,11 @@
                             @if($agenda->information != null)
                             <td class="px-4 py-2">
                                 @if(strlen($agenda->information) > 50)
-                                    <a href="#" data-modal-target="info-modal-{{ $agenda->id }}" data-modal-toggle="info-modal-{{ $agenda->id }}">
-                                        {{ \Illuminate\Support\Str::limit($agenda->information, 50, '...') }}
-                                    </a>
+                                   <button class="hover:text-blue-400" href="#" data-modal-target="crud-modal-{{ $agenda->id }}" data-modal-toggle="crud-modal-{{ $agenda->id }}">
+                                        {{ \Illuminate\Support\Str::limit($agenda->information, 50, '.....') }}
+                                    </button>
                                 @else
-                                    {{ $agenda->information }}
+                                    <button class="hover:text-blue-400" data-modal-target="crud-modal-{{ $agenda->id }}" data-modal-toggle="crud-modal-{{ $agenda->id }}">{{ $agenda->information }}</button>
                                 @endif
                             </td>
                             @else
@@ -97,40 +97,9 @@
                 @endif
             </tbody>
         </table>
-        <!-- Pagination Links -->
         <div class="p-4">
             {{ $agendas->links() }}
         </div>
-        @foreach ($agendas as $agenda)
-        <div id="info-modal-{{ $agenda->id }}" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-            <div class="relative p-4 w-full max-w-md max-h-full">
-                <!-- Modal content -->
-                <div class="relative p-4 w-full max-w-2xl max-h-full">
-                    <!-- Modal content -->
-                    <div class="relative bg-white rounded-lg shadow ">
-                        <!-- Modal header -->
-                        <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t ">
-                            <h3 class="text-xl font-semibold text-gray-900">
-                                Keterangan
-                            </h3>
-                            <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="info-modal-{{ $agenda->id }}">
-                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                                </svg>
-                                <span class="sr-only">Close modal</span>
-                            </button>
-                        </div>
-                        <!-- Modal body -->
-                        <div class="p-4 md:p-5 space-y-4">
-                            <p class="text-base leading-relaxed text-gray-500">
-                                {{ $agenda->information }}
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> 
-        @endforeach
         <!-- Main modal -->
         @foreach ($agendas as $agenda)
         <div id="crud-modal-{{ $agenda->id }}" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
@@ -156,7 +125,7 @@
                         <div class="grid gap-4 mb-4 grid-cols-2">
                             <div class="col-span-2">
                                 <label for="keterangan" class="block mb-2 text-sm font-medium text-gray-900">Keterangan</label>
-                                <textarea id="keterangan" name="keterangan" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="..."></textarea>                    
+                                <textarea id="keterangan-{{ $agenda->id }}" name="keterangan" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="...">{{ $agenda->information }}</textarea>                
                             </div>
                         </div>
                         <button type="submit" class="text-white inline-flex items-center bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
